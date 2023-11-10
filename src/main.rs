fb1bfc2fea59c869f257b7a903553447bf9610d5
noah <nomaxx117@gmail.com>
2023-11-10 17:32:27 -0600

    observability: change service label to mastodon

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ async fn main() {
         .init();
 
     PrometheusBuilder::new()
-        .add_global_label("service", "freighter")
+        .add_global_label("service", "mastodon")
         .with_http_listener(args.metrics)
         .set_buckets(&[
             100e-6, 500e-6, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 2e-1, 3e-1, 4e-1, 5e-1, 6e-1, 7e-1, 8e-1,
