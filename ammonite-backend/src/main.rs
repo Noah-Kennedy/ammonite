ce8929ccd35f234d96469afcd22ad78072f63c3f
noah <nomaxx117@gmail.com>
2023-11-12 13:10:12 -0600

    observability: absolutely explode our metrics cardinality

--- ammonite-backend/src/main.rs
+++ ammonite-backend/src/main.rs
@@ -108,7 +108,11 @@ async fn observe<B>(request: Request<B>, next: Next<B>) -> Response {
         )
     }
 
-    histogram!("response_processing_time_seconds", delta);
+    histogram!(
+        "response_processing_time_seconds", delta,
+        "status" => response.status().to_string(),
+        "method" => method.to_string(),
+    );
 
     response
 }
@@ -125,8 +129,10 @@ async fn main() {
         .add_global_label("service", "mastodon")
         .with_http_listener(args.metrics)
         .set_buckets(&[
-            100e-6, 500e-6, 1e-3, 5e-3, 1e-2, 5e-2, 1e-1, 2e-1, 3e-1, 4e-1, 5e-1, 6e-1, 7e-1, 8e-1,
-            9e-1, 1.0, 5.0, 10.0,
+            1e-3, 2e-3, 3e-3, 4e-3, 5e-3, 6e-3, 7e-3, 8e-3, 9e-3, // ms
+            1e-2, 2e-2, 3e-2, 4e-2, 5e-2, 6e-2, 7e-2, 8e-2, 9e-2, // 10s ms
+            1e-1, 2e-1, 3e-1, 4e-1, 5e-1, 6e-1, 7e-1, 8e-1, 9e-1, // 100s ms
+            1e0, 2e0, 3e0, 4e0, 5e0, 6e0, 7e0, 8e0, 9e0, // s
         ])
         .unwrap()
         .install()
